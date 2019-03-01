16cb64947a90feb323934fe4e63ea239b13c68cf
danielflorez111 <danielflorez111@hotmail.com>
2019-02-28 22:26:06 -0500

    React icons

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,9 +2,24 @@ import React from "react";
 
 import "./footer.scss";
 
+import { FaFacebook, FaYoutube, FaTwitter } from "react-icons/fa";
+
 const Footer = () => (
     <footer className="footer" align="center">
-        © {new Date().getFullYear()} Tisa Technology
+        <div>
+            © {new Date().getFullYear()} Tisa Technology
+        </div>
+        <div>
+            <a href="https://www.google.com.co">
+                <FaFacebook className="facebook-icon" />
+            </a>
+            <a href="https://www.google.com.co">
+                <FaYoutube className="youtube-icon" />
+            </a>
+            <a href="https://www.google.com.co">
+                <FaTwitter className="twitter-icon" />
+            </a>
+        </div>
     </footer>
 )
 
