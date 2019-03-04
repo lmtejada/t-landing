fce18024e581c77af13baf590c039f3d678d0a09
danielflorez111 <danielflorez111@hotmail.com>
2019-03-03 21:11:43 -0500

    Plans

--- src/components/plans/planCard.js
+++ src/components/plans/planCard.js
@@ -5,16 +5,14 @@ import Button from 'react-bootstrap/Button';
 import "./planCard.scss";
 
 const PlanCard = (props) => (
-    <Card className="text-center plan">
-        <Card.Header>Featured</Card.Header>
+    <Card className={`text-center plan ${props.plan}`}>
+        <Card.Header>{props.plan}</Card.Header>
         <Card.Body>
-            <Card.Title>Special title treatment</Card.Title>
-            <Card.Text>
-                With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>{props.price}</Card.Title>
+            {props.children}
+            <Button>Seleccionar</Button>
         </Card.Body>
-        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        {/* <Card.Footer>{props.price}</Card.Footer> */}
     </Card>
 )
 
--- src/components/plans/plans.js
+++ src/components/plans/plans.js
@@ -4,14 +4,51 @@ import Container from 'react-bootstrap/Container';
 import PlanCard from "./planCard";
 import CardDeck from "react-bootstrap/CardDeck";
 
+import { MdErrorOutline, MdDone } from "react-icons/md";
+
 import "./plans.scss";
 
+const getMaxPatients = (plan) => {
+    switch (plan) {
+        case 'free':
+            return <li><MdErrorOutline className="warning" />Máximo 20 pacientes</li>
+        case 'silver':
+            return <li><MdErrorOutline className="warning" />Máximo 100 pacientes</li>
+        case 'gold':
+            return <li><MdDone className="check" />Pacientes ilimitados</li>
+        default:
+            break;
+    }
+}
+
+const FeaturesList = ({ plan }) => {
+    return (
+        <ul className="list-unstyled">
+            <li><MdDone className="check" />Ingresar nuevos diagnósticos</li>
+            <li><MdDone className="check" />Ingresar nuevas evoluciones</li>
+            <li><MdDone className="check" />Consultar diagnósticos</li>
+            <li><MdDone className="check" />Consultar evoluciones</li>
+            <li><MdDone className="check" />Exportar Reportes - RIPS</li>
+            <li><MdDone className="check" />Garantía sobre la información</li>
+            <li><MdDone className="check" />Soporte por medio de correos</li>
+            <li><MdDone className="check" />Creación de pacientes nuevos</li>
+            {getMaxPatients(plan)}
+        </ul>
+    )
+};
+
 const Plans = () => (
-    <Container className="plans col-md-12">
+    <Container className="plans col-md-10">
         <CardDeck>
-            <PlanCard />
-            <PlanCard />
-            <PlanCard />
+            <PlanCard plan="free" price="0 /mes" >
+                <FeaturesList plan="free" />
+            </PlanCard>
+            <PlanCard plan="silver" price="$19.900 /mes">
+                <FeaturesList plan="silver" />
+            </PlanCard>
+            <PlanCard plan="gold" price="$23.900 /mes" >
+                <FeaturesList plan="gold" />
+            </PlanCard>
         </CardDeck>
     </Container>
 )
