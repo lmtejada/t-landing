6991b248be8edcc1ff596ef884a90e663a17ac1f
Laura Tejada <laura_mtc@hotmail.com>
2019-03-11 22:38:47 -0500

    Adding clients component and changing responsive behavior for product card

--- src/components/clients/clients.js
+++ src/components/clients/clients.js
@@ -2,9 +2,22 @@ import React from "react";
 
 import Container from 'react-bootstrap/Container';
 
+import "./clients.scss";
+import logo1 from '../../images/sample-logo.jpg';
+import logo2 from '../../images/Logo_Plasgara.png';
+
 const Clients = () => (
-    <Container className="profiles col-sm-12">
-        Clients
+    <Container className="clients col-sm-12">
+        <h1 className="text-center">Nuestros clientes</h1>
+        <hr className="divider" />
+        <div className="row content d-flex align-items-center justify-content-center">
+            <div className="col-sm-12 col-md-4 col-lg-3">
+                <img src={logo1} alt="Logo" />
+            </div>
+            <div className="col-sm-12 col-md-4 col-lg-3">
+                <img src={logo2} alt="Logo" />
+            </div>
+        </div>
     </Container>
 )
 
--- src/components/product/productCard.js
+++ src/components/product/productCard.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./productCard.scss";
 
 const ProductCard = (props) => (
-    <div className="product col-sm-12">
+    <div className="productCard col-sm-12">
         <div className="row content">
             <div className="col-lg-8 col-md-12 text">
                 <h2>{props.title}</h2>
                 <div className="text">{props.children}</div>
             </div>
-            <div className="col-lg-4 col-md-12 image">
+            <div className="col-lg-4 col-md-12 image d-flex align-items-center justify-content-center">
                 <img
                     src={props.image}
                     alt="Product"
